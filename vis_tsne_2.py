0662c764189f6a24eb5e311b99ee290235117977
lizuyao2010 <zuyaoli2010@gmail.com>
2015-09-29 11:40:14 -0700

    debug

--- vis_tsne_2.py
+++ vis_tsne_2.py
@@ -9,8 +9,7 @@ from tsne_python import tsne
 
 # import some data to play with
 X,Y = datasets.load_data()
-X=X[500:]
-Y=Y[500:]
+print X[0]
 ini_dim= 50
 X_reduced = tsne.tsne(X, 2, ini_dim, 20.0)
 
