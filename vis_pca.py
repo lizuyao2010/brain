1cebc6e748e18773fe8ebc1a8aa758c0db6c3ab0
lizuyao2010 <zuyaoli2010@gmail.com>
2015-09-29 20:44:17 -0700

    pca under sampling ration plot eigenvalues

--- vis_pca.py
+++ vis_pca.py
@@ -17,16 +17,28 @@ X,Y = datasets.load_data()
 # scaler = preprocessing.StandardScaler().fit(X)
 # X=scaler.transform(X)
 
-# Generate the new dataset using under-sampling method
-verbose = False
-# 'Random under-sampling'
-US = UnderSampler(verbose=verbose)
-X, Y = US.fit_transform(X, Y)
 # To getter a better understanding of interaction of the dimensions
 # plot the first three PCA dimensions
 fig = plt.figure(1, figsize=(8, 6))
 ax = Axes3D(fig, elev=-150, azim=110)
-X_reduced = PCA(n_components=3).fit_transform(X)
+pca=PCA()
+X_reduced = pca.fit_transform(X)
+
+# plt.figure(1, figsize=(4, 3))
+# plt.clf()
+# plt.axes([.2, .2, .7, .7])
+# plt.plot(pca.explained_variance_, linewidth=2)
+# plt.axis('tight')
+# plt.xlabel('n_components')
+# plt.ylabel('explained_variance_')
+
+# Generate the new dataset using under-sampling method
+verbose = False
+# 'Random under-sampling'
+# ratio of majority elements to sample with respect to the number of minority cases.
+US = UnderSampler(ratio=1.,verbose=verbose)
+X_reduced, Y = US.fit_transform(X_reduced, Y)
+
 ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=Y, cmap=plt.cm.Paired)
 ax.set_title("First three PCA directions")
 ax.set_xlabel("1st eigenvector")
@@ -35,7 +47,7 @@ ax.set_ylabel("2nd eigenvector")
 ax.w_yaxis.set_ticklabels([])
 ax.set_zlabel("3rd eigenvector")
 ax.w_zaxis.set_ticklabels([])
-fig.savefig("pic/pca_3_r")
+fig.savefig("pic/pca_3_t")
 # To getter a better understanding of interaction of the dimensions
 # plot the first two PCA dimensions
 x_min, x_max = X_reduced[:, 0].min() - .5, X_reduced[:, 0].max() + .5
@@ -51,5 +63,5 @@ plt.ylim(y_min, y_max)
 plt.xticks(())
 plt.yticks(())
 
+plt.savefig("pic/pca_2_t")
 # plt.show()
-plt.savefig("pic/pca_2_r")
